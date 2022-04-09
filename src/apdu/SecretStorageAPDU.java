6cb8d86fac1b9f0748bf36d7cff13e2668c51c57
DaudNaveed <46792573+DaudNaveed@users.noreply.github.com>
2022-04-09 20:40:21 +0200

    List available keys functionality added

---
+++ src/apdu/SecretStorageAPDU.java
@@ -0,0 +1,236 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package jcsim;
+import javacardss.SecretStorageApplet;
+import cardTools.CardManager;
+import cardTools.RunConfig;
+import cardTools.Util;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import javax.smartcardio.CardException;
+
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+
+/**
+ *
+ * @author zargham ahmad
+ */
+public class Jcsim {
+private static String APPLET_AID = "A000000408000010000100";
+    //private static String APPLET_AID2 = "0102030405060708090102";
+    private static byte APPLET_AID_BYTE[] = Util.hexStringToByteArray(APPLET_AID);
+
+    private static final String STR_APDU_INCORRECT_PIN = "B02000000431323333";
+    
+    private static final String STR_APDU_PIN_HEADER = "B0200000";
+    private static final String STR_APDU_INSERT_KEY_VALUE_HEADER = "B030";
+    private static final String STR_APDU_GET_VALUE_HEADER = "B040";
+    private static final String STR_APDU_GET_LIST = "B0500000";
+    
+    private static final String STR_APDU_UNSUPPORTED_INS = "B0E1000000"; // This instruction is not supported by the card
+
+    /**
+     * Main entry point.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            Jcsim main = new Jcsim();
+            
+            main.demo();
+            
+        } catch (Exception ex) {
+            System.out.println("Exception : " + ex);
+        }
+    }
+
+    public void demo() throws Exception {
+        // CardManager abstracts from real or simulated card, provide with applet AID
+        final CardManager cardMngr = new CardManager(true, APPLET_AID_BYTE);          
+        
+        // Get default configuration for subsequent connection to card (personalized later)
+        final RunConfig runCfg = RunConfig.getDefaultConfig();
+
+        // A) If running on physical card
+        //runCfg.setTestCardType(RunConfig.CARD_TYPE.PHYSICAL); // Use real card
+
+        // B) If running in the simulator 
+        runCfg.setAppletToSimulate(SecretStorageApplet.class); // main class of applet to simulate
+        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL); // Use local simulator
+
+        // Connect to first available card
+        // NOTE: selects target applet based on AID specified in CardManager constructor
+        System.out.print("Connecting to card...");
+        if (!cardMngr.Connect(runCfg)) {
+            System.out.println(" Failed.");
+        }
+        System.out.println(" Done.");
+
+        int in_action = -1;
+        Scanner sc = new Scanner(System.in);
+        
+        checkPIN(cardMngr);
+        
+        while(in_action != 0) {
+            System.out.println("What do you want to do?\n1)Change PIN\n2)Add secret\n3)Get secret\n4)List keys\n0)Finish");
+            in_action = sc.nextInt();
+            
+            switch(in_action) {
+                case 1: 
+                    break;
+                case 2: 
+                    addSecret(cardMngr);
+                    break;
+                case 3: 
+                    retrieveValue(cardMngr);
+                    break;
+                case 4: 
+                    listKeys(cardMngr);
+                    break;
+                default:
+                    System.out.println("Unsupported operation!");
+                    break;
+            }
+        }
+        
+    }
+
+    private void addSecret(CardManager cardMngr) throws CardException {
+        Scanner sc = new Scanner(System.in);
+        
+        int format_key = getFormat("key");
+        System.out.print("Key: ");
+        String key = sc.nextLine();
+        
+        int format_value = getFormat("value");
+        System.out.print("Value: ");
+        String value = sc.next();
+        
+        key = convertFormatToHex(key, format_key);
+        value = convertFormatToHex(value, format_value);
+                
+        int key_len = key.length();
+        int value_len = value.length();
+        
+        String apdu = String.format("%s%02x%02x%02xF1%sF2%s", 
+                STR_APDU_INSERT_KEY_VALUE_HEADER,
+                key_len/2,
+                value_len/2,
+                (key_len + value_len)/2 + 2,
+                key,
+                value);
+        
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(apdu)));
+        
+        if(response.getSW() != 0x9000) {
+            System.err.println("Incorrect key-value!\n" + response.getSW());
+            System.exit(1);
+        }
+        
+    }
+
+    private void retrieveValue(CardManager cardMngr) throws CardException, UnsupportedEncodingException {
+        Scanner sc = new Scanner(System.in);
+        
+        int format_key = getFormat("key");
+        System.out.print("Key: ");
+        String key = sc.nextLine();
+        
+        key = convertFormatToHex(key, format_key);
+        int key_len = key.length();
+        
+        String apdu = String.format("%s%02x00%02x%s", 
+                STR_APDU_GET_VALUE_HEADER,
+                key_len/2,
+                key_len/2,
+                key);
+        
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(apdu)));
+        
+        if(response.getSW() != 0x9000) {
+            System.err.println("Incorrect key-value!\n" + response.getSW());
+            System.exit(1);
+        }
+        
+        String value = new String(response.getData(), "UTF-8");
+        System.out.println("Value:" + value + "\n");
+    }
+    
+    private void listKeys(CardManager cardMngr) throws CardException, UnsupportedEncodingException {
+        Scanner sc = new Scanner(System.in);
+        
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(STR_APDU_GET_LIST)));
+        
+        if(response.getSW() != 0x9000) {
+            System.err.println("Incorrect key-value!\n" + response.getSW());
+            System.exit(1);
+        }      
+       System.out.println(response);
+        String keys = new String(response.getData(), "UTF-8");        
+        String[] res =keys.split(" ");          
+         if(res.length == 0) {
+            System.out.println("No keys available.");
+        } else {
+            System.out.println("Available keys: ");
+            System.out.println(keys + "\n");       
+         }    
+    }
+    
+
+    private void checkPIN(CardManager cardMngr) throws CardException {  
+        Scanner sc = new Scanner(System.in);      
+        
+        System.out.print("Enter PIN please (in hex): ");
+        String pin = sc.nextLine();
+        String pin_apdu = buildPinAPDU(pin);
+        System.out.println(pin_apdu);
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(pin_apdu)));
+        
+        if(response.getSW() != 0x9000) {
+            System.err.println("Wrong PIN!\n");
+            System.exit(1);
+        }
+    }    
+
+    private String buildPinAPDU(String pin) {
+        String apdu = new String();
+        int pin_len = pin.length()/2; // dividing by 2 because it is in hex; will change later
+        
+        return String.format("%s%02x%s", STR_APDU_PIN_HEADER, pin_len, pin);
+    }
+    
+    private int getFormat(String s) {
+        Scanner sc = new Scanner(System.in);
+        int format;        
+        
+        do {
+            System.out.println("Which format is your " + s + " in?\n1)hex\n2)string\n3)bin");
+            format = sc.nextInt();
+        } while(format > 3 || format < 0);
+        
+        return format;
+    }
+
+    private String convertFormatToHex(String value, int format) {
+        switch(format) {
+            case 1: //hex
+                return value;
+            case 2: // string
+                return Util.toHex(value.getBytes());
+            case 3: // bin
+                System.err.println("Unsupported for now!");
+                System.exit(1);
+            default:
+                System.err.println("Unsupported format!");
+                System.exit(1);
+        }
+        
+        return "";
+    }
+
+}
--- src/javacardss/SecretStorageApplet.java
+++ src/javacardss/SecretStorageApplet.java
@@ -12,18 +12,60 @@ public class SecretStorageApplet extends Applet {
     private static final byte[] OK_PIN_MSG = {'P', 'i', 'n', ' ', 'O', 'K', '!'};
     private static final byte[] BAD_PIN_MSG = {'B', 'A', 'D', ' ', 'P', 'I', 'N', '!'};
 
+<<<<<<< Updated upstream
     private final static short SW_WRONG_PIN = 0x63c0;
 
     // INS byte for the command that verifies the PIN (from ISO7816-4)
     private final static byte INS_VERIFY = 0x20;
+=======
+    final static byte APPLET_CLA = (byte) 0xB0;
+    
+    //exceptions 
+    final static short SW_Exception = (short) 0xff01;
+    final static short SW_ArrayIndexOutOfBoundsException = (short) 0xff02;
+    final static short SW_ArithmeticException = (short) 0xff03;
+    final static short SW_ArrayStoreException = (short) 0xff04;
+    final static short SW_NullPointerException = (short) 0xff05;
+    final static short SW_NegativeArraySizeException = (short) 0xff06;
+    final static short SW_CryptoException_prefix = (short) 0xf100;
+    final static short SW_SystemException_prefix = (short) 0xf200;
+    final static short SW_PINException_prefix = (short) 0xf300;
+    final static short SW_TransactionException_prefix = (short) 0xf400;
+    final static short SW_CardRuntimeException_prefix = (short) 0xf500;
+    
+    //error return codes
+    private final static short SW_WRONG_PIN = 0x63c0;
+    public final static short SW_DUPLICATE_KEY = 0x6a8a;
+    public final static short SW_NO_EXISTING_VALUE = 0x6dfa;
+    
+    //instruction codes
+    private final static byte INS_VERIFY = 0x20;
+    public final static byte INS_ADD_KEYVALUE_ENTRY = 0x30;
+    public final static byte INS_LIST_KEYS = 0x50;
+    public final static byte INS_GET_VALUE = 0x40;
+>>>>>>> Stashed changes
 
     public final static byte PIN_TRY_LIMIT = 0x03;
     public final static byte USER_PIN_SIZE = 0x04;
     private static byte[] default_pin = {0x31, 0x32, 0x33, 0x34};
+<<<<<<< Updated upstream
     
     private SecretStorageApplet() {
         m_user_pin = new OwnerPIN((byte) 3, (byte) 4);
         m_user_pin.update(default_pin, (short) 0x00, USER_PIN_SIZE);
+=======
+
+    // Tag byte for key
+    public final static byte TAG_KEY = (byte) 0xF1;
+    // Tag byte for secret records
+    public final static byte TAG_SECRETVALUE = (byte) 0xF2;
+
+    private KeyValueRecord current;
+
+    private SecretStorageApplet() {
+        m_user_pin = new OwnerPIN((byte) 3, (byte) 4);
+        m_user_pin.update(default_pin, (short) 0x00, PIN_SIZE);
+>>>>>>> Stashed changes
     }
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {
@@ -35,8 +77,101 @@ public class SecretStorageApplet extends Applet {
         if (selectingApplet()) {
             return;
         }
+<<<<<<< Updated upstream
+=======
+        
+        byte[] buffer = apdu.getBuffer();
+                
+        if (!m_user_pin.isValidated() && buffer[ISO7816.OFFSET_INS] != INS_VERIFY)
+            ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);
+
+        try {
+            if (buffer[ISO7816.OFFSET_CLA] == APPLET_CLA) {
+                switch (buffer[ISO7816.OFFSET_INS]) {
+                    case INS_VERIFY:
+                        verifyPin(apdu);
+                        break;
+                    case INS_ADD_KEYVALUE_ENTRY:
+                        addSecretValue(apdu);
+                        break;
+                    case INS_GET_VALUE:
+                        getValue(apdu);
+                        break;
+                     case INS_LIST_KEYS:
+                        listAvailableKeys(apdu);
+                        break;    
+
+                    default:
+                        ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+                        break;
+                }
+            } else {
+                ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+            }
+        } catch (ISOException e) {
+            throw e; // Our exception from code, just re-emit
+        } catch (ArrayIndexOutOfBoundsException e) {
+            ISOException.throwIt(SW_ArrayIndexOutOfBoundsException);
+        } catch (ArithmeticException e) {
+            ISOException.throwIt(SW_ArithmeticException);
+        } catch (ArrayStoreException e) {
+            ISOException.throwIt(SW_ArrayStoreException);
+        } catch (NullPointerException e) {
+            ISOException.throwIt(SW_NullPointerException);
+        } catch (NegativeArraySizeException e) {
+            ISOException.throwIt(SW_NegativeArraySizeException);
+        } catch (CryptoException e) {
+            ISOException.throwIt((short) (SW_CryptoException_prefix | e.getReason()));
+        } catch (SystemException e) {
+            ISOException.throwIt((short) (SW_SystemException_prefix | e.getReason()));
+        } catch (PINException e) {
+            ISOException.throwIt((short) (SW_PINException_prefix | e.getReason()));
+        } catch (TransactionException e) {
+            ISOException.throwIt((short) (SW_TransactionException_prefix | e.getReason()));
+        } catch (CardRuntimeException e) {
+            ISOException.throwIt((short) (SW_CardRuntimeException_prefix | e.getReason()));
+        } catch (Exception e) {
+            ISOException.throwIt(SW_Exception);
+        }
+    }
+
+    private void verifyPin(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        
+        if (!m_user_pin.check(buffer, ISO7816.OFFSET_CDATA, PIN_SIZE))
+            ISOException.throwIt(SW_WRONG_PIN);
+            
+        // this isn't necessary
+        Util.arrayCopyNonAtomic(OK_PIN_MSG, (short) 0, buffer, ISO7816.OFFSET_CDATA, (short)OK_PIN_MSG.length);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (short)OK_PIN_MSG.length);
+    }
+    
+    void checkTLV(byte[] buffer, short inOfs,
+                   byte tag, short len, short maxLen) {
+        if (buffer[inOfs] != tag)
+            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+
+        if (len > maxLen)
+            ISOException.throwIt(ISO7816.SW_DATA_INVALID);
+    }
+    
+    
+    void getValue(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        byte key_size = buffer[ISO7816.OFFSET_P1];
+        KeyValueRecord record = KeyValueRecord.search(buffer, ISO7816.OFFSET_CDATA, key_size);
+        
+        if(record == null)
+            ISOException.throwIt(SW_NO_EXISTING_VALUE);
+        
+        byte value_len = record.getSecretValue(buffer, (byte) ISO7816.OFFSET_CDATA);
+        
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, value_len);
+    }
+>>>>>>> Stashed changes
 
         byte[] buffer = apdu.getBuffer();
+<<<<<<< Updated upstream
 
         switch (buffer[ISO7816.OFFSET_INS]) {
             case INS_VERIFY:
@@ -63,6 +198,61 @@ public class SecretStorageApplet extends Applet {
             Util.arrayCopyNonAtomic(BAD_PIN_MSG, (short) 0, apdubuf, (short) 0, msgLength);
             apdu.setOutgoingAndSend((short) 0, msgLength);
         }
+=======
+        byte key_size = buffer[ISO7816.OFFSET_P1];
+        byte value_size = buffer[ISO7816.OFFSET_P2];
+
+        if (key_size == 0 || value_size == 0)
+            ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+        
+        if (apdu.setIncomingAndReceive() != (short) (buffer[ISO7816.OFFSET_LC] & 0xFF))
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+
+        short ofsKey = ISO7816.OFFSET_CDATA;
+        short ofsSecretValue = (short) (ofsKey + 1 + key_size);
+        checkTLV(buffer, ofsKey, TAG_KEY, key_size, KeyValueRecord.SIZE_KEY);
+        checkTLV(buffer, ofsSecretValue, TAG_SECRETVALUE, value_size, KeyValueRecord.SIZE_VALUE);
+
+        // Check that key does not exist
+        if (KeyValueRecord.search(buffer, (short) (ofsKey + 1), key_size) != null)
+            ISOException.throwIt(SW_DUPLICATE_KEY);
+
+        // Allocates and initializes a key value pair entry
+        JCSystem.beginTransaction();
+        KeyValueRecord record = KeyValueRecord.getInstance();
+        record.setKey(buffer, (short) (ofsKey + 1), key_size);
+        record.setSecretValue(buffer, (short) (ofsSecretValue + 1), value_size);
+        JCSystem.commitTransaction();
+    } 
+    
+    void listAvailableKeys(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        // Checks P1 and initializes the "current" value
+        if (buffer[ISO7816.OFFSET_P1] == 0)
+            current = KeyValueRecord.getFirst();
+        else if ((buffer[ISO7816.OFFSET_P1] != 1) || (current == null))
+            ISOException.throwIt(ISO7816.SW_INCORRECT_P1P2);
+
+        // Builds the response
+        short offset = 0;
+        while (current != null) {
+            // Checks that the identifier record fits in the APDU
+            // WARNING: assumes a 256-byte APDU buffer
+            byte len = current.getKeyLength();
+            if ((short) ((short) (offset + len)) > 255)
+                break;
+
+            // Copies the identifier in the buffer
+            buffer[offset++] = TAG_KEY;
+            buffer[offset++] = len;
+            current.getKey(buffer, offset);
+
+            // Gest to the next record
+            offset += len;
+            current = current.getNext();
+        }
+        apdu.setOutgoingAndSend((short) 0, offset);
+>>>>>>> Stashed changes
     }
 
 }
